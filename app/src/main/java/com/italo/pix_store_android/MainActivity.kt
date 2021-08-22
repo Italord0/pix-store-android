37a8e74c17f186586df036a9d3b9b76c7b443c28
Italo Melo <italord.melo@gmail.com>
2021-08-22 02:14:54 -0300

    first commit

---
+++ app/src/main/java/com/italo/pix_store_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.italo.pix_store_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
